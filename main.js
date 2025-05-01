0b0c3e636cc7482abf82be4a5a3f955d2a5cb302
pareeksahab <maheshsharmasumit@gmail.com>
2025-05-01 15:24:35 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadURL(
+    process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '/build/index.html')}`
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
